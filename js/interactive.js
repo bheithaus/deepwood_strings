242d71d75546d9294b72ff5d80e0c55d69c9a010
Brian Heithaus <bheithaus@gmail.com>
2013-06-14 17:57:59 -0700

    initial commit

---
+++ js/interactive.js
@@ -0,0 +1,13 @@
+$(function(){
+	console.log(document.title);
+	highlightCurrentTab();
+	
+	function highlightCurrentTab() {
+		var sections = document.title.split(" | "),
+			activeTab;
+			
+		$("ul.nav li").removeClass("active");
+		activeTab = sections.length > 1 ? sections[0].toLowerCase() : "home";
+		$("#"+ activeTab +"Link").addClass("active");
+	}	
+});
