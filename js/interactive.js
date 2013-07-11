8cd2eb8e86cecfc314644776f711c6bb6ca9c34e
Brian Heithaus <bheithaus@gmail.com>
2013-07-11 16:36:20 -0700

    add images and restyle

--- js/interactive.js
+++ js/interactive.js
@@ -1,7 +1,5 @@
 $(function(){
-	console.log(document.title);
 	highlightCurrentTab();
-	$(".musicians a").popover();
 	function highlightCurrentTab() {
 		var sections = document.title.split(" | "),
 			activeTab;
